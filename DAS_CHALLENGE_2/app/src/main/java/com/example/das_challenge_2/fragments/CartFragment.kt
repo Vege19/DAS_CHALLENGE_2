eee49a0264ac9a155ac52a77350a4edf79371c3a
_vegeuwu <josueardon.developer@gmail.com>
2019-11-04 23:29:20 -0600

    Finding products with code scanner

--- DAS_CHALLENGE_2/app/src/main/java/com/example/das_challenge_2/fragments/CartFragment.kt
+++ DAS_CHALLENGE_2/app/src/main/java/com/example/das_challenge_2/fragments/CartFragment.kt
@@ -1,25 +1,29 @@
 package com.example.das_challenge_2.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.das_challenge_2.R
+import com.example.das_challenge_2.activities.ScannerActivity
 import com.example.das_challenge_2.adapters.CartAdapter
 import com.example.das_challenge_2.models.ProductModel
 import com.example.das_challenge_2.utils.getFirebaseReference
 import com.example.das_challenge_2.utils.showToast
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.fragment_cart.*
+import androidx.appcompat.widget.Toolbar;
 
 class CartFragment : Fragment() {
 
     private var products: MutableList<ProductModel> = mutableListOf()
     private lateinit var adapter: CartAdapter
+    private var codebarString: String = ""
+    private var cartTotal = 0.0
+    private var stock = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,7 +35,31 @@ class CartFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        activity?.title = "Carrito"
+        openScannerActivity()
+
+        //Getting cart total cost
+        FirebaseDatabase.getInstance().getReference("cart/cart_total").addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                showToast(requireContext(), p0.message)
+            }
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.exists()) {
+                    cartTotal = p0.getValue(Double::class.java)!!
+                }
+            }
+        })
+
+        //Getting product stock
+        getFirebaseReference("products/$codebarString/product_stock").addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                showToast(requireContext(), p0.message)
+            }
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.exists()) {
+                    stock = (p0.getValue(Int::class.java)!!)
+                }
+            }
+        })
 
         cartRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         adapter = CartAdapter(products, requireContext())
@@ -114,6 +142,80 @@ class CartFragment : Fragment() {
 
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.option_scan -> {
+                startActivityForResult(Intent(requireContext(), ScannerActivity::class.java), 1)
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
 
+    private fun openScannerActivity() {
+        cartToolbar.setOnMenuItemClickListener(object: Toolbar.OnMenuItemClickListener {
+            override fun onMenuItemClick(item: MenuItem?): Boolean {
+                return when (item?.itemId) {
+                    R.id.option_scan -> {
+                        startActivityForResult(Intent(requireContext(), ScannerActivity::class.java), 1)
+                        true
+                    }
+                    else -> {
+                        false
+                    }
+                }
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                codebarString = data?.data.toString()
+                showToast(requireContext(), codebarString)
+                foundProduct()
+            }
+        }
+
+    }
+
+    private fun foundProduct() {
+        val productsReference = getFirebaseReference("products")
+
+        productsReference.addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                showToast(requireContext(), p0.message)
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.exists()) {
+                    for (tmp in p0.children) {
+                        //Find product according product id
+                        val product = tmp.getValue(ProductModel::class.java)
+                        //Log.d("Debug", "\nFirebaseProductId: ${product?.product_id}\nBarcodeId: $codebarString")
+                        if (product?.product_id == codebarString) {
+                            addFoundProductToCart(product)
+                        }
+                    }
+                }
+            }
+        })
+
+        getFirebaseReference("products/$codebarString/product_stock").setValue(stock)
+
+    }
+
+    private fun addFoundProductToCart(product: ProductModel) {
+        val cartProductsReference = getFirebaseReference("cart/cart_products")
+        val cartProductId = cartProductsReference.push().key
+        product.product_cart_id = cartProductId!!
+        cartProductsReference.child(cartProductId).setValue(product)
+        //Update price
+        getFirebaseReference("cart/cart_total").setValue(cartTotal + product.product_price)
+    }
 
 }
